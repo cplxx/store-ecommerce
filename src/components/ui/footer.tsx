110b357a61dc8939dd99ddea71bd96459ccc9019
Yan Castro <yancastrohenrique1@gmail.com>
2023-10-27 23:29:09 -0300

    fix: vercel

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
       <p>
-        © 2023 Copyright <span className="font-semibold">Store</span>
+        © 2023 Copyright <span className="font-semibold">Stor</span>
       </p>
     </div>
   );
